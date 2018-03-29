f9a2838843efe3a2454877027cd744f3ae1e4c66
Toshko Andreev <toshko@smart-ss.org>
2018-03-29 10:14:34 +0300

    commit

---
+++ CoreXSD/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace CoreXSD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cpFile cp = new cpFile();
+            Protocol_DrugTherapy therapy = new Protocol_DrugTherapy();
+            therapy.Part_Chemotherapy = new Protocol_DrugTherapyPart_Chemotherapy();
+            therapy.Part_Chemotherapy.Gen_Markers = new Protocol_DrugTherapyPart_ChemotherapyGen_Markers();
+            therapy.Part_Chemotherapy.Gen_Markers.Name = new string[2] { "One", "Two" };
+            therapy.Part_Chemotherapy.Gen_Markers.Sign = new bool[2] { true, false };
+            cp.Protocol_DrugTherapy = new Protocol_DrugTherapy[1] { therapy };
+
+            using (var output = new FileStream("output.xml", FileMode.Create))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(cpFile)); 
+                xml.Serialize(output, cp);
+                output.Flush();
+            }
+        }
+    }
+}
